f8720f3bd1149de1346161e78d217c9ae0c40c25
Chris <chrisiviedev@gmail.com>
2026-03-01 03:38:30 +0000

    Added generics.go and updated struct-embedding.go

--- GoByExample/struct-embedding.go
+++ GoByExample/struct-embedding.go
@@ -23,6 +23,8 @@ type container struct {
 
 func main() {
 
+    // When creating structs with literals, we have to initialzie the embedding explicitly; 
+    // Here the embedded type servers as the field name.
     co := container{
         base: base{
             num: 1,
@@ -30,16 +32,22 @@ func main() {
         str: "some name",
     }
 
+    // We can access the base fields directly on co, e.g co.num. 
     fmt.Printf("co={num: %v, str: %v}\n", co.num, co.str)
 
+    // Alternatively, we can spell out the full path using the embedded type name.
     fmt.Println("also num:", co.base.num)
 
+    // Since container embeds base, the methods of base also become methods of a container. 
+    // Here we invoke a method that was embedded from base directly on co.
     fmt.Println("describe:", co.describe())
 
     type describer interface {
         describe() string
     }
 
+    // Embedding structs with methods may be used to bestow interface implementations onto other structs. 
+    // Here we see that a container now implements the describer interface because it embeds base.
     var d describer = co
     fmt.Println("describer:", d.describe())
 }
